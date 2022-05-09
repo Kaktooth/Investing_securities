a7b01c2b37e80fb2ba250c0c823723b1c1c02d70
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-05-09 13:43:53 +0300

    create project

---
+++ src/main/java/com/investing/securities/InvestingSecuritiesApplication.java
@@ -0,0 +1,13 @@
+package com.investing.securities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingSecuritiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestingSecuritiesApplication.class, args);
+    }
+
+}
