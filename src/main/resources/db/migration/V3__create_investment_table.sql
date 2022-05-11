d90b1e45d11e1051f39db973cf54c2ec88e41a19
Vitalii Dubovyk <vitaliy.dubovyk.1@gmail.com>
2022-05-11 15:16:14 +0300

    delete cascade

--- src/main/resources/db/migration/V3__create_investment_table.sql
+++ src/main/resources/db/migration/V3__create_investment_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE investment
     financial_quotation bigint,
     date_of_purchase    timestamp,
     date_of_sale        timestamp,
-    securities_id       UUID REFERENCES "securities",
-    customer_id         UUID REFERENCES "customer"
+    securities_id       UUID REFERENCES "securities" ON DELETE CASCADE,
+    customer_id         UUID REFERENCES "customer" ON DELETE CASCADE
 );
